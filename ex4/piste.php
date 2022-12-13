3ce34e380519af9b2632d81c165928369184afc9
yannicl <yannic.luyckx@gmail.com>
2022-12-12 21:10:18 -0500

    ajout jeu de piste

---
+++ ex4/piste.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Lab 1">
+    <meta name="author" content="Yannic Luyckx">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body class="">
+
+    <?php
+   
+
+    ?>
+    <main>
+        <?php ?>
+            <div class="alert alert-success" role="alert">
+                Vous avez trouvé! C'était bien (4, 3).
+            </div>
+        <?php ?>
+            <div class="alert alert-info" role="alert">
+                Vous êtes à ?? pas de l'objectif.
+            </div>
+        <?php ?>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-4">
+                </div>
+                <div class="col-4">
+                    <a class="btn btn-primary" href="piste.php?x=&y=" role="button">Haut</a>
+                </div>
+                <div class="col-4">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <a class="btn btn-primary" href="piste.php?x=&y=" role="button">Gauche</a>
+                </div>
+                <div class="col-4">
+                    <a class="btn btn-primary" href="piste.php?x=&y=" role="button">Bas</a>
+                </div>
+                <div class="col-4">
+                    <a class="btn btn-primary" href="piste.php?x=&y=" role="button">Droite</a>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+</body>
+
+</html>
