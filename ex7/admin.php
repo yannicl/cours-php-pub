af153d99c8333e6c3b11e8a9a52aeca737ceca81
Yannic Luyckx <yannic.luyckx@gmail.com>
2023-01-17 20:57:45 -0500

    ajout version initiale pour ex7

---
+++ ex7/admin.php
@@ -0,0 +1,81 @@
+<?php
+
+// vérification des droits
+// si aucun utilisateur en session, logout
+// si utilisateur non admin, logout
+
+
+// traitement des actions
+// action de bloquer un utilisateur
+// action de verifier un utilisateur
+
+require_once('db.admin.inc.php');
+
+
+
+// préparation de la vue
+
+$users = findAllUsers();
+
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Exercice creation d'une page d'administration des utilisateurs">
+    <meta name="author" content="">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+
+<body class="">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Admin Section</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">nom</th>
+                <th scope="col">courriel</th>
+                <th scope="col">statut</th>
+                <th scope="col">action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($users as $user) : ?>
+                <tr>
+                    <td><?= $user->id ?></td>
+                    <td><?= $user->name ?></td>
+                    <td><?= $user->email ?></td>
+                    <td><?= $user->status ?></td>
+                    <td>Bloquer || Verifier</td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+</body>
+
+</html>
---
+++ ex7/db.admin.inc.php
@@ -0,0 +1,42 @@
+<?php 
+require_once '../../../config.php';
+
+// XAMPP
+//const DB_HOST = "localhost";
+//const DB_NAME = "php_test";
+//const DB_USER = "root";
+//const DB_PASS = "";
+
+$dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
+} catch (PDOException $e) {
+     throw new PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+function findAllUsers() {
+     global $pdo;
+     // à compléter
+     return $products;
+ }
+
+ function updateUserStatus($userId, $newStatus) {
+     global $pdo;
+     // à compléter
+ }
+
+ function blockUser($userId) {
+     updateUserStatus($userId, "blocked");
+ }
+
+ function verifyUser($userId) {
+     updateUserStatus($userId, "verified");
+ }
+
+
+?>
