34f91543b877bd7d054883be5cb0edc725275d1a
Yannic Luyckx <yannic.luyckx@gmail.com>
2024-01-03 21:06:45 -0500

    ajout exercice sur les formulaires

---
+++ ex4/addition.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<main>
+    <form action="addition.php" method="post">
+        +<input type="text" id="a" name="a"><br>
+        +<input type="text" id="b" name="b">
+        <input type="submit" value="Calculer">
+    </form>
+    <hr>
+    =
+</main>
+
+</body>
+
+</html>
